5e00c128d971be4ea3a35ccacbd458e220d7f041
Gauravi Sharma <gauravisharma1508@gamil.com>
2022-04-16 17:07:52 +0530

    again

--- cafe/settings.py
+++ cafe/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_STORAGE = 'whitenoise.storage.CompressionManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
